48cb96c7d321e766d5833631361d23598365ab7f
Geoff Harcourt <geoff.harcourt@gmail.com>
2017-10-10 08:48:23 -0400

    Streamline round flow

--- lib/drafter.rb
+++ lib/drafter.rb
@@ -5,9 +5,10 @@ require "drafter/choice"
 require "drafter/potential_slots_for_picker"
 
 class Drafter
-  def initialize(candidates:, pickers:, slot_counts:)
+  def initialize(candidates:, pickers:, slot_counts:, debug: false)
     @candidates = candidates.sort_by { |candidate| candidate[:value] }.reverse
     @slot_counts = slot_counts
+    @debug = debug
 
     @results = Array.new(pickers) { Drafter::Picker.new }
   end
@@ -19,29 +20,27 @@ class Drafter
 
   private
 
-  attr_reader :candidates, :results, :slot_counts
+  attr_reader :candidates, :debug, :results, :slot_counts
 
   def make_choices
-    round = 1
+    round_number = 1
     loop do
-      time = Time.now
-      puts ""
-      puts "Round #{round}"
-      results.each { |picker| make_pick_for_picker(picker) }
-      puts "...finished in #{Time.now - time} seconds"
-      puts ""
-      round += 1
-
-      time = Time.now
-      puts ""
-      puts "Round #{round}"
-      results.reverse_each { |picker| make_pick_for_picker(picker) }
-      puts "...finished in #{Time.now - time} seconds"
-      puts ""
-      round += 1
+      make_picks_for_round(results, round_number)
+      round_number += 1
+      make_picks_for_round(results.reverse, round_number)
+      round_number += 1
     end
   rescue NoFurtherCandidates
     # noop
+  end
+
+  def make_picks_for_round(pickers_in_order, round_number)
+    time = Time.now
+    debug_puts ""
+    debug_puts "Round #{round_number}"
+    pickers_in_order.each { |picker| make_pick_for_picker(picker) }
+    debug_puts "...finished in #{Time.now - time} seconds"
+    debug_puts ""
   end
 
   def make_pick_for_picker(picker)
@@ -49,11 +48,17 @@ class Drafter
     top_candidate = Choice.new(candidates: candidates, slot_counts: slot_counts, picker: picker).top_candidate
 
     if top_candidate
+      debug_puts "#{top_candidate[:name]}, took #{Time.now - time}s"
       picker.picks << top_candidate
-      puts "#{top_candidate[:name]}, took #{Time.now - time}s"
       @candidates -= [top_candidate]
     else
       fail NoFurtherCandidates
+    end
+  end
+
+  def debug_puts(text)
+    if debug
+      puts text
     end
   end
 
